0e6b0627668e244cdd510a4b70a84ce067df2c55
BorisSavianov <bsavianov@gmail.com>
2024-01-18 16:09:15 +0200

    changes before noit

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: "https://codehub-project.com/",
+  siteUrl: "https://codehub-project.vercel.app/",
   generateRobotsTxt: true,
   sitemapSize: 7000,
 };
